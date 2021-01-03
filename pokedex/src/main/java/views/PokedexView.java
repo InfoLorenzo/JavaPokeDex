7a0a71e04186cfafdefa02daeaeef4a58f8da592
Lorenzo <infolorenzogomez@gmail.com>
2021-01-03 19:38:52 +0100

    Añadido fondo de Pokedex

--- pokedex/src/main/java/views/PokedexView.java
+++ pokedex/src/main/java/views/PokedexView.java
@@ -1,6 +1,13 @@
 package views;
 
 import javax.swing.JPanel;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
@@ -10,11 +17,29 @@ public class PokedexView extends JPanel {
 	 * Create the panel.
 	 */
 	public PokedexView() {
+
 		setLayout(null);
-		
-		JLabel lblBackground = new JLabel("New label");
-		lblBackground.setHorizontalAlignment(SwingConstants.CENTER);
+
+		// Start -- Background
+
+		JLabel lblBackground = new JLabel("No hay foto");
+
+		try {
+			lblBackground = new JLabel(new ImageIcon(ImageIO.read(new URL("https://i.imgur.com/Lr4arcd.png"))));
+			lblBackground.setVerticalAlignment(SwingConstants.BOTTOM);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		lblBackground.setBounds(0, 0, 718, 469);
+		
+		// End -- Background
+
+		
 		add(lblBackground);
 
 	}
